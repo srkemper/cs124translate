8908ea8e6347823e671053d23157a2bcaac530fb
sethjhs <sethjhs@stanford.edu>
2014-02-25 14:29:53 -0800

    tranlation.py updated dictionary updated corpus character change

--- translate.py
+++ translate.py
@@ -5,6 +5,39 @@ import getopt
 import os
 import math
 
+def noun_of_the_noun_switch(translation_list):
+	switch = 1
+	while switch!=0:
+		for idx, word in enumerate(translation_list):
+			firstN =""
+			fi = -1
+			of = False
+			oi = -1
+			the = False
+			ti = -1
+			secondN = ""
+			si = -1
+			if "/N" in word or "/NP" in word and fi==-1:
+				firstN=word
+				fi=idx
+			if word == "of/PREP" and fi!=-1:
+				of = True
+				oi=idx
+			if word == "of_the/PREP" and fi!=-1:
+				of = True
+				oi=idx
+				ti=idx
+				the = True
+			if fi!=-1 and (word!="of/PREP" or word!="of_the/PREP"):
+				firstN=""
+				fi=-1
+			if fi!=-1 and oi!=-1 and word=="the/PREP":
+				ti=idx
+				the=True
+			if "/N" in word or "/NP" in word and fi==-1:
+
+
+
 def loadList(file_name):
     """Loads text files as lists of lines. Used in evaluation."""
     with open(file_name) as f:
@@ -26,7 +59,7 @@ def main():
 		translations = []
 		for idx, word in enumerate(entry_list):
 			if idx== 0: 
-				key = word
+				key = word.lower()
 			else:
 				translations.append(word)
 		dictionary[key]=translations
@@ -34,20 +67,28 @@ def main():
 
 	for idx, sentence in enumerate(sentences_lists):
 		print("")
-		print("Sentence ",idx)
+		print("Sentence ",idx+1)
 		sentence_list = sentence.split()
 		translation_list = []
 		for word in sentence_list:
-			print(word)
+			#print(word)
 			word = word.replace('.','')
 			word = word.replace(',','')
-			
+			word = word.replace(':','')
+			word = word.replace('(','')
+			word = word.replace(')','')
+			word = word.replace('-','')
 			word = word.lower()
 			print(word)
-			trans = dictionary.get(word)
+			if word!='':
+				trans = dictionary.get(word)
+				translation_list.append(trans[0])
+			
+			translation_list = noun_of_the_noun_switch(translation_list)
+			
+			translation_list= noun_adjective_switch(translation_list)
 
-			translation_list.append(trans[0])
-		print translation_list
+		print(translation_list)
 
 
 
