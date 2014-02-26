91290f795de828955b9778f8256d91f255d8df89
sethjhs <sethjhs@stanford.edu>
2014-02-25 16:59:13 -0800

    merge

--- translate.py
+++ translate.py
@@ -3,41 +3,59 @@ import sys
 import getopt
 import os
 import math
-import nltk
-from nltk.corpus import cess_esp
-from nltk import UnigramTagger, BigramTagger, TrigramTagger, HiddenMarkovModelTagger
+#import nltk
+#from nltk.corpus import cess_esp
+#from nltk import UnigramTagger, BigramTagger, TrigramTagger, HiddenMarkovModelTagger
+import re
+from random import randint
+
+def remove_pos_tags_and_underscores(translation_list):
+	new_list = []
+	for word in translation_list:
+		words = word.split('/')
+		new_list.append(words[0])
+	final_list = []
+	for word in new_list:
+		words = word.split('_')
+		for tok in words:
+			final_list.append(tok)
+	return final_list
+
+
+def noun_adjective_switch(translation_list):
+	switch = 1
+	regex = "(.*) (\w*/NP?) (\w*)/ADJ? (.*)"
+	trans = " ".join(translation_list)
+	while switch!=0:
+		switch =0
+		EntityMatches = re.findall(regex,trans)
+		if(len(EntityMatches))>0:
+			switch =1
+			for idx, match in enumerate(EntityMatches):
+				#print trans
+				trans = EntityMatches[idx][0]+" "+ EntityMatches[idx][2]+"_"+EntityMatches[idx][1]+" "+EntityMatches[idx][3]
+				#print trans
+				#print EntityMatches[idx][2]+"_"+EntityMatches[idx][1]
+	translation_list = trans.split()
+	return translation_list
+
 
 def noun_of_the_noun_switch(translation_list):
 	switch = 1
+	regex = "(.*) (\w*/NP?) of[ _the/PREP]* (\w*)/NP? (.*)"
+	trans = " ".join(translation_list)
 	while switch!=0:
-		for idx, word in enumerate(translation_list):
-			firstN =""
-			fi = -1
-			of = False
-			oi = -1
-			the = False
-			ti = -1
-			secondN = ""
-			si = -1
-			if "/N" in word or "/NP" in word and fi==-1:
-				firstN=word
-				fi=idx
-			if word == "of/PREP" and fi!=-1:
-				of = True
-				oi=idx
-			if word == "of_the/PREP" and fi!=-1:
-				of = True
-				oi=idx
-				ti=idx
-				the = True
-			if fi!=-1 and (word!="of/PREP" or word!="of_the/PREP"):
-				firstN=""
-				fi=-1
-			if fi!=-1 and oi!=-1 and word=="the/PREP":
-				ti=idx
-				the=True
-			if "/N" in word or "/NP" in word and fi==-1:
-
+		switch =0
+		EntityMatches = re.findall(regex,trans)
+		if(len(EntityMatches))>0:
+			switch =1
+			for idx, match in enumerate(EntityMatches):
+				#print trans
+				trans = EntityMatches[idx][0]+" "+ EntityMatches[idx][2]+"_"+EntityMatches[idx][1]+" "+EntityMatches[idx][3]
+				#print trans
+				#print EntityMatches[idx][2]+"_"+EntityMatches[idx][1]
+	translation_list = trans.split()
+	return translation_list
 
 
 def loadList(file_name):
@@ -47,12 +65,12 @@ def loadList(file_name):
     return l
 
 def main():
-	tagged_corpus = cess_esp.tagged_sents()
-	size = int(len(tagged_corpus) * .9)
-	training = tagged_corpus[:size]
-	print "training HiddenMarkovModelTagger"
-	hmm_tagger = HiddenMarkovModelTagger.train(training)
-	print "finished training"
+	#tagged_corpus = cess_esp.tagged_sents()
+	#size = int(len(tagged_corpus) * .9)
+	#training = tagged_corpus[:size]
+	#print "training HiddenMarkovModelTagger"
+	#hmm_tagger = HiddenMarkovModelTagger.train(training)
+	#print "finished training"
 
 
 	dict_file = "./data/dictionary.txt"
@@ -77,14 +95,17 @@ def main():
 	tagged_sentences = []
 	for idx, sentence in enumerate(sentences_lists):
 		if sentence == "": continue
-		tagged_sentences.append(hmm_tagger.tag(sentence.split()))
+		#tagged_sentences.append(hmm_tagger.tag(sentence.split()))
 		print("")
+
 
 		print("Sentence ",idx+1)
 
 		sentence_list = sentence.split()
-		print sentence_list
-		translation_list = []
+
+		full_translation_list_of_lists = []
+		demo_translation_list = []
+
 		for word in sentence_list:
 			# print(word)
 			word = word.replace('.','')
@@ -94,23 +115,28 @@ def main():
 			word = word.replace(')','')
 			word = word.replace('-','')
 			word = word.lower()
-
-			print(word)
+			#print(word)
 			if word!='':
 				trans = dictionary.get(word)
-				translation_list.append(trans[0])
-			
-			translation_list = noun_of_the_noun_switch(translation_list)
-			
-			translation_list= noun_adjective_switch(translation_list)
-
-		print(translation_list)
+#				for translation in trans:
+#					if idx==0:
+#						full_translation_list_of_lists.append(word)
+#					else:
+						#for lest in full_translation_list_of_lists:
+							#lest.
+		#print(translation_list)
 		
-	print tagged_sentences
-
-
-
-
+		#print tagged_sentences
+			demo_translation_list.append(trans[(idx+3)%len(trans)])
+			
+		pos_free_translation_list = remove_pos_tags_and_underscores(demo_translation_list)
+		print("Initial Translation: ",' '.join(pos_free_translation_list),)
+		
+		translation_list= noun_adjective_switch(demo_translation_list)
+		translation_list = noun_of_the_noun_switch(demo_translation_list)
+		
+		pos_free_translation_list = remove_pos_tags_and_underscores(demo_translation_list)
+		print("Final Translation: ",' '.join(pos_free_translation_list),)
 
     #cp.train(clues, gold_parsed_clues)
     ##parsed_clues = cp.parseClues(clues)
